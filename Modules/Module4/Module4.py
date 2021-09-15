8a1772d38945d1d07e8b2e99881c106befc20cd3
CSI-Andres-Rodriguez <24-084@sanignacio.pr>
2021-09-15 10:39:34 -0400

    module 4

---
+++ Modules/Module4/Module4.py
@@ -0,0 +1,15 @@
+var:str = "regular string"
+myNumber:float = 3.5
+
+myFunctionalString = f"Combine an existing {var} with a number such as: {myNumber} or execute something like {round(3.4 * 1.1)}"
+
+print(myFunctionalString)
+
+print(f"Or use the print directly {myNumber}")
+
+meal=input("What did you have for Breakfast?")
+print=(meal)
+meal=input("What was your Lunch?")
+print=(meal)
+meal=input("Have you had dinner?")
+print=(meal)
