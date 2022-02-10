bc8b70fe79cea9a8c982f2de8f066a719440bf8e
CSI-Andres-Rodriguez <89204946+CSI-Andres-Rodriguez@users.noreply.github.com>
2022-02-10 10:32:14 -0400

    hangman

---
+++ Projects/Hangman/Hangman.py
@@ -0,0 +1,19 @@
+import json, ssl
+import os
+from pathlib import Path
+import urllib.request
+from Cannabis import Cannabis
+
+# This is discouraged but it will avoid certificate validation (prevents error)
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# This is the URL from which we will request the data
+cannabisURL = "https://random-data-api.com/api/cannabis/random_cannabis"
+
+
+req = urllib.request.Request(cannabisURL)
+r = json.loads(urllib.request.urlopen(req).read())
+
+cannabis:Cannabis = Cannabis(**r)
+
+print(cannabis.strain)
