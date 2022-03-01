e648b7ccba54c5daec5650d7675f1cfbd3907379
CSI-Andres-Rodriguez <89204946+CSI-Andres-Rodriguez@users.noreply.github.com>
2022-03-01 10:57:30 -0400

    hangman

--- Projects/Hangman/Hangman.py
+++ Projects/Hangman/Hangman.py
@@ -1,27 +1,114 @@
 import json, ssl
 import urllib.request
 from Cannabis import Cannabis
+import urllib.request
 
+#characters used list
+used=[]
+
+def get_cannabis():
 # This is discouraged but it will avoid certificate validation (prevents error)
-ssl._create_default_https_context = ssl._create_unverified_context
+    ssl._create_default_https_context = ssl._create_unverified_context
 
 # This is the URL from which we will request the data
-cannabisURL = "https://random-data-api.com/api/cannabis/random_cannabis"
+    cannabisURL = "https://random-data-api.com/api/cannabis/random_cannabis"
+
+#Api request
+    req = urllib.request.Request(cannabisURL)
+    r = json.loads(urllib.request.urlopen(req).read())
+
+    cannabis:Cannabis = Cannabis(**r)
+
+    return cannabis.strain.upper()
 
 
-req = urllib.request.Request(cannabisURL)
-r = json.loads(urllib.request.urlopen(req).read())
+# create var to store the value of get_cannabis()
 
-
-cannabis:Cannabis = Cannabis(**r)
+myCannabis = get_cannabis()
 
 IncorrectLetters = []
 
-print(len(cannabis.strain) * "_ ")
+print(len(myCannabis) * "_ ")
 
- #print(cannabis.strain)
+#drawings of my hangman
 
+hang = ["""
+H A N G M A N - 
+
+   <====>
+   |   l
+       l
+       l
+       l
+       l
+<=======>""", 
+"""
+H A N G M A N - 
+
+  <====>
+  I   l
+  *   l
+      l
+      l
+      l
+<=======>""", 
+"""
+H A N G M A N -
+
+  <====>
+  I    l
+  *    l
+  I    l
+       l
+       l
+<=======>""", 
+"""
+H A N G M A N - 
+
+  <====>
+  I    l
+  *    l
+ $I    l
+       l
+       l
+=========""", 
+"""
+H A N G M A N - 
+
+  <====>
+  I    l
+  *    l
+ $I$   l
+       l
+       l
+<=======>""", 
+"""
+H A N G M A N - 
+
+  <====>
+  I    l
+  *    l
+ $I$   l
+ ( )   l
+       l
+<=======>""", 
+"""
+H A N G M A N - 
+
+ <=====>
+  I    l
+  *    l
+ $I$   l
+ ( )   l
+       l
+<=======>""",
+]
+
+#incorrect letters in my hangman
+print(myCannabis)
 def getInput():
+    invalid_characters = ("1","2","3","4","5","6","7","8","9","0","!","@","#","$","%","^","&","*","(",")","{","}","[","]",";","'","<",",",".",">","/","?","|")
+    
     while(True):
         guess=input("Your guess is")
         if guess .isnumeric()== True :
@@ -40,9 +127,33 @@ def getInput():
             print("Letter is already used")
             continue
 
-        IncorrectLetters.append(guess)
+        IncorrectLetters.append(guess) 
         return guess
 
-print(getInput())
-    
+# print(getInput())
 
+def printword():
+    temp:str=""
+    len(myCannabis)
+    for letter in myCannabis :
+        print(letter)
+        # letter in(IncorrectLetters)
+        if letter not in IncorrectLetters :
+            temp+="_"
+        else:
+            temp+=letter
+
+        return temp
+
+def printStep():
+    counter = 0
+    for letter in used:
+        if letter not in myCannabis:
+         counter= counter + 1
+
+    print(hang[counter])
+
+while True :
+    printStep()
+    getInput()
+    printword()
