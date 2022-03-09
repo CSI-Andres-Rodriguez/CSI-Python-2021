e40d7cbb94a7710fdc7a61529b291cb83e721b0f
CSI-Andres-Rodriguez <89204946+CSI-Andres-Rodriguez@users.noreply.github.com>
2022-03-09 10:22:11 -0400

    1 block

---
+++ Projects/Snake/Snake.py
@@ -0,0 +1,15 @@
+import pygame
+import time
+import random
+ 
+pygame.init()
+ 
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
+brown = (92, 42, 42)
+pink = (238, 32, 224)
+purple = (135, 32, 238)
