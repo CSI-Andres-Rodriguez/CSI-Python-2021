0cb57b980f506bde52a02aea9cfa2a4c31feb3f5
CSI-Andres-Rodriguez <89204946+CSI-Andres-Rodriguez@users.noreply.github.com>
2022-03-30 20:42:03 -0400

    snake
    me falta darle commented a algunas cosas

--- Projects/Snake/Snake.py
+++ Projects/Snake/Snake.py
@@ -1,57 +1,90 @@
-# access the script from another Python file
+# acces the script from another Python file
 import pygame
+# Allows to work with time in Python
+import time
 #initiates the game
 pygame.init()
-#the size of the display of the game
-dis=pygame.display.set_mode((400,300))
-#it refresh the game and the screen
-pygame.display.update()
+ 
+ # Different RBG. RGB describes a color as a tuple of three components.
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+yellow = (255, 255, 102)
+green = (0, 255, 0)
+blue = (50, 153, 213)
+brown = (92, 42, 42)
+pink = (238, 32, 224)
+purple = (135, 32, 238)
+
+#The dis module includes functions for working with Python bytecode by “disassembling” it into a more human-readable form.
+#width
+dis_width = 600
+#height
+dis_height  = 500
+
+#This control displays a list of items for the user to select. The items parameter must be a Python list. It can contain as many items as you wish.
 # 
-pygame.quit()
-
-# access the script from another Python file
-import pygame
-#initiates the game
-pygame.init() 
-#the size of the display of the game
-dis=pygame.display.set_mode((400,300)) 
-#name of the game
-pygame.display.set_caption('Snake game by Edureka')
-# it will do a variable name "game over" that has a value of false
-game_over=False
-# if the value was not false then do a loop
-while not game_over: 
-    # event is a temporary variable that represent every variable on the pygame list (forloop)
-    for event in pygame.event.get():
-        # if the game find an envent that finished the game and change te value of the varieable to true
-        if event.type==pygame.QUIT:
-            # the value of the variable is true
-            game_over=True
-        # if the value is not false then do a loop and prints out all the actions that take place on the screen
-        print(event)
-# game finished
-
-#white = (255, 255, 255) 
-#yellow = (255, 255, 102)
-#black = (0, 0, 0)
-#red = (213, 50, 80)
-#green = (0, 255, 0)
-#blue = (50, 153, 213)
-#brown = (92, 42, 42)
-#pink = (238, 32, 224)
-#purple = (135, 32, 238)
-  
-
-import pygame
-pygame.init()
-dis=pygame.display.set_mode((400,300))
-pygame.display.update()
-pygame.display.set_caption('Snake game by Edureka')
-game_over=False
+dis = pygame.display.set_mode((dis_width, dis_width))
+#Shows or display the name of the game (Snake Game by Edureka)
+pygame.display.set_caption('Snake Game by Edureka')
+ 
+ #it will do a variable name "game over" that has a value of false
+game_over = False
+ 
+x1 = dis_width/2
+y1 = dis_height/2
+ 
+snake_block=15  
+ 
+x1_change = 0
+y1_change = 0
+ 
+ #Clock of Snake Game by Edureka
+clock = pygame.time.Clock()
+#The speed of the snake is 20
+snake_speed=20
+ 
+font_style = pygame.font.SysFont(None, 50)
+ 
+ #Define the message
+def message(msg,color): 
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [dis_width/2, dis_height/2])
+ 
 while not game_over:
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            game_over=True
+        if event.type == pygame.QUIT:
+            game_over = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x1_change = -snake_block
+                y1_change = 0
+            elif event.key == pygame.K_RIGHT:
+                x1_change = snake_block
+                y1_change = 0
+            elif event.key == pygame.K_UP:
+                y1_change = -snake_block
+                x1_change = 0
+            elif event.key == pygame.K_DOWN:
+                y1_change = snake_block
+                x1_change = 0
+ 
+    if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+        game_over = True
+ 
+    x1 += x1_change
+    y1 += y1_change
+    dis.fill(white)
+    pygame.draw.rect(dis, black, [x1, y1, snake_block, snake_block])
+ 
+    pygame.display.update()
+ 
+    clock.tick(snake_speed)
+ 
+message("You lost",red)
+pygame.display.update()
+time.sleep(2)
  
 pygame.quit()
 quit()
+
